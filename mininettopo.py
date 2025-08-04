b4f6b3122e7b2c81c5982654160a19a8d2d81ab8
shiny-li <cerasifera_li@outlook.com>
2025-08-04 00:27:34 -0700

    add room, change topo and ryu

--- mininettopo.py
+++ mininettopo.py
@@ -16,6 +16,12 @@ def hotel_wifi_topology():
     h1 = net.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
     h2 = net.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
     h3 = net.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
+    h4 = net.addHost('h4', ip='10.0.0.11/24', mac='00:00:00:00:00:0a')
+    h5 = net.addHost('h5', ip='10.0.0.12/24', mac='00:00:00:00:00:0b')
+    h6 = net.addHost('h6', ip='10.0.0.13/24', mac='00:00:00:00:00:0c')
+    h7 = net.addHost('h7', ip='10.0.0.21/24', mac='00:00:00:00:00:15')
+    h8 = net.addHost('h8', ip='10.0.0.22/24', mac='00:00:00:00:00:16')
+    h9 = net.addHost('h9', ip='10.0.0.23/24', mac='00:00:00:00:00:17')
 
     # 添加路由器（代替服务器）
     router = net.addHost('router', ip='10.0.0.254/24', mac='00:00:00:00:00:AA')
@@ -24,6 +30,12 @@ def hotel_wifi_topology():
     net.addLink(h1, s1, port1=0, port2=2)
     net.addLink(h2, s1, port1=0, port2=3)
     net.addLink(h3, s1, port1=0, port2=4)
+    net.addLink(h4, s1, port1=0, port2=5)
+    net.addLink(h5, s1, port1=0, port2=6)
+    net.addLink(h6, s1, port1=0, port2=7)
+    net.addLink(h7, s1, port1=0, port2=8)
+    net.addLink(h8, s1, port1=0, port2=9)
+    net.addLink(h9, s1, port1=0, port2=10)
     net.addLink(router, s1, port1=0, port2=1)
 
     # 启动网络
@@ -33,6 +45,12 @@ def hotel_wifi_topology():
     h1.cmd('route add default gw 10.0.0.254')
     h2.cmd('route add default gw 10.0.0.254')
     h3.cmd('route add default gw 10.0.0.254')
+    h4.cmd('route add default gw 10.0.0.254')
+    h5.cmd('route add default gw 10.0.0.254')
+    h6.cmd('route add default gw 10.0.0.254')
+    h7.cmd('route add default gw 10.0.0.254')
+    h8.cmd('route add default gw 10.0.0.254')
+    h9.cmd('route add default gw 10.0.0.254')
     
     # 配置路由器接口和ARP
     router.cmd('ifconfig router-eth0 10.0.0.254/24 hw ether 00:00:00:00:00:AA up')
@@ -47,6 +65,12 @@ def hotel_wifi_topology():
     h1.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
     h2.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
     h3.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h4.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h5.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h6.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h7.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h8.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
+    h9.cmd('echo "nameserver 8.8.8.8" > /etc/resolv.conf')
 
     # 启动CLI
     CLI(net)
