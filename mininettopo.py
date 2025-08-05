a61e32c6ed1cbd6bf0dc57b5bb10388345026119
黄廷旭 <huangtingxu@qq.com>
2025-08-05 20:26:50 +1000

    失败的VLAN

--- mininettopo.py
+++ mininettopo.py
@@ -28,42 +28,59 @@ def hotel_wifi_topology():
     
     # 连接所有节点
     net.addLink(h1, s1, port1=0, port2=2)
-    h1.cmd("ip link add link h1-eth0 name h1.101 type vlan id 101")
-    h1.cmd("ip addr add 10.0.11.1/24 dev h1.101")
-    h1.cmd("ip link set up h1.101")
     net.addLink(h2, s1, port1=0, port2=3)
-    h2.cmd("ip link add link h2-eth0 name h2.102 type vlan id 102")
-    h2.cmd("ip addr add 10.0.12.2/24 dev h2.102")
-    h2.cmd("ip link set up h2.102")
     net.addLink(h3, s1, port1=0, port2=4)
-    h3.cmd("ip link add link h3-eth0 name h3.103 type vlan id 103")
-    h3.cmd("ip addr add 10.0.13.3/24 dev h3.103")
-    h3.cmd("ip link set up h3.103")
     net.addLink(h4, s1, port1=0, port2=5)
-    h4.cmd("ip link add link h4-eth0 name h4.201 type vlan id 201")
-    h4.cmd("ip addr add 10.0.21.11/24 dev h4.201")
-    h4.cmd("ip link set up h4.201")
     net.addLink(h5, s1, port1=0, port2=6)
-    h5.cmd("ip link add link h5-eth0 name h5.202 type vlan id 202")
-    h5.cmd("ip addr add 10.0.22.12/24 dev h5.202")
-    h5.cmd("ip link set up h5.202")
     net.addLink(h6, s1, port1=0, port2=7)
-    h6.cmd("ip link add link h6-eth0 name h6.203 type vlan id 203")
-    h6.cmd("ip addr add 10.0.23.13/24 dev h6.203")
-    h6.cmd("ip link set up h6.203")
     net.addLink(h7, s1, port1=0, port2=8)
-    h7.cmd("ip link add link h7-eth0 name h7.301 type vlan id 301")
-    h7.cmd("ip addr add 10.0.31.21/24 dev h7.301")
-    h7.cmd("ip link set up h7.301")
     net.addLink(h8, s1, port1=0, port2=9)
-    h8.cmd("ip link add link h8-eth0 name h8.302 type vlan id 302")
-    h8.cmd("ip addr add 10.0.32.22/24 dev h8.302")
-    h8.cmd("ip link set up h8.302")
     net.addLink(h9, s1, port1=0, port2=10)
-    h9.cmd("ip link add link h9-eth0 name h9.303 type vlan id 303")
-    h9.cmd("ip addr add 10.0.33.23/24 dev h9.303")
-    h9.cmd("ip link set up h9.303")
     net.addLink(router, s1, port1=0, port2=1)
+    
+
+    
+
+    # 启动网络
+    net.start()
+    
+    # ---------------- VLAN 配置 ----------------
+    # Router trunk
+    s1.cmd("ovs-vsctl set port s1-eth1 trunks=101,102,103,201,202,203,301,302,303")
+    
+    # Host access ports
+    s1.cmd("ovs-vsctl set port s1-eth2 tag=101")
+    s1.cmd("ovs-vsctl set port s1-eth3 tag=102")
+    s1.cmd("ovs-vsctl set port s1-eth4 tag=103")
+    s1.cmd("ovs-vsctl set port s1-eth5 tag=201")
+    s1.cmd("ovs-vsctl set port s1-eth6 tag=202")
+    s1.cmd("ovs-vsctl set port s1-eth7 tag=203")
+    s1.cmd("ovs-vsctl set port s1-eth8 tag=301")
+    s1.cmd("ovs-vsctl set port s1-eth9 tag=302")
+    s1.cmd("ovs-vsctl set port s1-eth10 tag=303")
+
+    # ---------------- Host IP 配置 ----------------
+    h1.cmd("ip addr flush dev h1-eth0")
+    h1.cmd("ip addr add 10.0.11.1/24 dev h1-eth0")
+    h2.cmd("ip addr flush dev h2-eth0")
+    h2.cmd("ip addr add 10.0.12.2/24 dev h2-eth0")
+    h3.cmd("ip addr flush dev h3-eth0")
+    h3.cmd("ip addr add 10.0.13.3/24 dev h3-eth0")
+    h4.cmd("ip addr flush dev h4-eth0")
+    h4.cmd("ip addr add 10.0.21.11/24 dev h4-eth0")
+    h5.cmd("ip addr flush dev h5-eth0")
+    h5.cmd("ip addr add 10.0.22.12/24 dev h5-eth0")
+    h6.cmd("ip addr flush dev h6-eth0")
+    h6.cmd("ip addr add 10.0.23.13/24 dev h6-eth0")
+    h7.cmd("ip addr flush dev h7-eth0")
+    h7.cmd("ip addr add 10.0.31.21/24 dev h7-eth0")
+    h8.cmd("ip addr flush dev h8-eth0")
+    h8.cmd("ip addr add 10.0.32.22/24 dev h8-eth0")
+    h9.cmd("ip addr flush dev h9-eth0")
+    h9.cmd("ip addr add 10.0.33.23/24 dev h9-eth0")
+
+
+    # ---------------- Router VLAN 子接口配置 ----------------
     router.cmd("ip link add link router-eth0 name router.101 type vlan id 101")
     router.cmd("ip addr add 10.0.11.254/24 dev router.101")
     router.cmd("ip link set up router.101")
@@ -100,22 +117,17 @@ def hotel_wifi_topology():
     router.cmd("ip addr add 10.0.33.254/24 dev router.303")
     router.cmd("ip link set up router.303")
 
-
-    s1.cmd("ovs-vsctl set port s1-eth1 trunks=101,102,103,201,202,203,301,302,303")
-
-    # 启动网络
-    net.start()
     
     # 配置主机的默认网关指向路由器
-    h1.cmd('ip route add default via 10.0.11.254 dev h1.101')
-    h2.cmd('ip route add default via 10.0.12.254 dev h2.102')
-    h3.cmd('ip route add default via 10.0.13.254 dev h3.103')
-    h4.cmd('ip route add default via 10.0.21.254 dev h4.201')
-    h5.cmd('ip route add default via 10.0.22.254 dev h5.202')
-    h6.cmd('ip route add default via 10.0.23.254 dev h6.203')
-    h7.cmd('ip route add default via 10.0.31.254 dev h7.301')
-    h8.cmd('ip route add default via 10.0.32.254 dev h8.302')
-    h9.cmd('ip route add default via 10.0.33.254 dev h9.303')
+    h1.cmd('ip route add default via 10.0.11.254')
+    h2.cmd('ip route add default via 10.0.12.254')
+    h3.cmd('ip route add default via 10.0.13.254')
+    h4.cmd('ip route add default via 10.0.21.254')
+    h5.cmd('ip route add default via 10.0.22.254')
+    h6.cmd('ip route add default via 10.0.23.254')
+    h7.cmd('ip route add default via 10.0.31.254')
+    h8.cmd('ip route add default via 10.0.32.254')
+    h9.cmd('ip route add default via 10.0.33.254')
     
     # 配置路由器接口和ARP
     router.cmd('ifconfig router-eth0 hw ether 00:00:00:00:00:AA up')
